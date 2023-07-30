0c58090ea79ddef94808a8a9dd37af83b4bbbf9e
Zan <zan@protokol.com>
2023-07-30 13:29:57 +0200

    Initial commit

---
+++ test/cdk-mbq.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMbq from '../lib/cdk-mbq-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-mbq-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMbq.CdkMbqStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
